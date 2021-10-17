b9809cd2a675c441c664bb3ff56748ead322a666
Kahuka0506 <50985609+Kahuka0506@users.noreply.github.com>
2021-10-18 01:24:03 +0900

    data structure

--- README.md
+++ README.md
@@ -30,14 +30,13 @@
 
 
 ## Data Structure
-- Union Find  `union_find.cpp`,
-- Union Find (重みつき) `union_find_wight.cpp`
-- Union Find (ポテンシャル) `union_find_potential.cpp`
+- Union Find  `union_find.cpp`
+- 重みつきUnion Find `union_find.cpp`
 - 座標圧縮 `compress.cpp`
 - Binary index tree `binary_index_tree.cpp`   
 - セグメント木  `segment_tree.cpp`
-- セグメント木 Range Minimum Query  `segment_tree_RMQ.cpp`
-- セグメント木 Range Sum Query  `segment_tree_RSQ.cpp`
+- セグメント木 Range Minimum Query  `segment_tree.cpp`
+- セグメント木 Range Sum Query  `segment_tree.cpp`
 - 遅延評価セグメント木 Range Updata Query  `lazy_segment_tree_RUQ.cpp`
 - 遅延評価セグメント木 Range Add Query  `lazy_segment_tree_RAQ.cpp`
 - トライ木 `trie.cpp`
--- data_structure/union_find.cpp
+++ data_structure/union_find.cpp
@@ -26,5 +26,57 @@ public:
     
     bool same(int a, int b){return find(a) == find(b);}
     
+    int size(int a) {return -root[find(a)];}
+};
+
+
+
+
+
+class WeightedUnionFind {
+    int N;
+    vi root;
+    vi w;
+    
+public:
+    WeightedUnionFind(int N_):N(N_){
+        root.assign(N,-1);
+        w.assign(N,0);
+    }
+    
+    int find(int a){
+        if(root[a] < 0) return a;
+        else {
+            int r = find(root[a]);
+            w[a] += w[root[a]];
+            return root[a] = r;
+        }
+    }
+    
+    int weight(int a){
+        find(a);
+        return w[a];
+    }
+    
+    void unite(int a, int b, int c){
+        c += (weight(a)-weight(b));
+        a = find(a), b = find(b);
+        if(a == b) return;
+        
+        if(-root[a] < -root[b]){
+            c = -c;
+            root[b] += root[a];
+            root[a] = b;
+            w[a] = c;
+        }else{
+            root[a] += root[b];
+            root[b] = a;
+            w[b] = c;
+        }
+    }
+    
+    bool same(int a, int b){return find(a) == find(b);}
+    
+    int diff(int a, int b){return weight(b)-weight(a);}
 };
 
