e1d811f132e0aeeb8146fa85e89cd485dfae9e32
Kahuka0506 <kou22uhneiom@icloud.com>
2022-10-11 23:48:29 +0900

    scc

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -1,5 +1,87 @@
 
 class SCC {
+   int N;
+   
+public:
+   vvi G,rG,g;
+   SCC(int n):N(n){
+       G.resize(N);
+       rG.resize(N);
+   }
+   void add_edge(int a, int b){
+       G[a].pb(b);
+       rG[b].pb(a);
+   }
+   
+   int cn;
+   vi vis,ord,cmp,deg;
+   vvi node;
+   void dfs(int u){
+       vis[u] = 1;
+       for (int v : G[u]) {
+           if(vis[v]) continue;
+           dfs(v);
+       }
+       ord.pb(u);
+   }
+   void rdfs(int u, int c){
+       cmp[u] = c;
+       node[c].pb(u);
+       for (int v : rG[u]) {
+           if(cmp[v] != -1) continue;
+           rdfs(v,c);
+       }
+   }
+   void solve(){
+       vis.assign(N,0);
+       ord.clear();
+       rep(i,N) if(vis[i] == 0) dfs(i);
+       cn = 0;
+       cmp.assign(N,-1);
+       per(i,N) if(cmp[ord[i]] == -1) {
+           node.pb({});
+           rdfs(ord[i],cn);
+           cn++;
+       }
+   }
+   void construct_dag(){
+       g.resize(cn);
+       node.resize(cn);
+       rep(u,N){
+           for (int v : G[u]) {
+               if(cmp[u] != cmp[v]) g[cmp[u]].pb(cmp[v]);
+           }
+       }
+       deg.assign(cn,0);
+       rep(i,cn){
+           sort(all(g[i]));
+           g[i].erase(unique(all(g[i])),g[i].end());
+           for (int j : g[i]) deg[j]++;
+       }
+   }
+   vi tp_sort(){
+       vi res;
+       queue<int> que;
+       rep(i,cn) if(deg[i] == 0) que.push(i);
+       while (!que.empty()) {
+           int u = que.front();
+           que.pop();
+           res.pb(u);
+           for (int v : g[u]) {
+               deg[v]--;
+               if(deg[v] == 0) que.push(v);
+           }
+       }
+       return res;
+   }
+   
+   
+};
+
+
+
+
+class SCC {
     int N;
     
 public:
